2f98fb7138b9c3ed8cb5cacc4e029b26163aa1a5
Chanthy Ngin <nginchanthy@gmail.com>
2018-05-08 12:06:34 -0700

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MineComponent } from './mine/mine.component';
+import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
+import { LedgerComponent } from './ledger/ledger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MineComponent,
+    BuyComponent,
+    SellComponent,
+    LedgerComponent
   ],
   imports: [
     BrowserModule,
